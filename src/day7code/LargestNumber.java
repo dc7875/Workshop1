e4a37519f9cc1d17f0d76e9a4fa94828af8f4bcd
Durgesh094 <durgeshchavan1994@gmail.com>
2021-11-24 20:31:04 +0530

    commiting

--- src/day7code/LargestNumber.java
+++ src/day7code/LargestNumber.java
@@ -1,10 +1,34 @@
 package day7code;
 
+import java.util.Scanner;
+
 public class LargestNumber {
 
 	public static void main(String[] args) {
+		int num1,num2,num3,largest;
+		int sum;
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter num1: ");
+		num1 = sc.nextInt();
+		System.out.println("Enter num2: ");
+		num2 = sc.nextInt();
+		System.out.println("Enter num3: ");
+		num3 = sc.nextInt();
+		
+		largest= num3 > (num1 > num2 ? num1 : num2) ? num3 : ((num1 > num2) ? num1 : num2); 
+		System.out.println("Largest Number is: " + largest);
+		
+		for(sum=0; largest!=0; largest=largest/10)  
+		{  
+		//finds the last digit and add it to the variable sum      
+		sum = sum + largest % 10;  
+		}  
+		//prints the result  
+		System.out.println("Sum of digits: "+sum);  
+	
+	}
 
 	}
 
-}
+
